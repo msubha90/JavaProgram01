7974c4ac9fd5a803cc94f8e1ddf320bc0c1933f1
msubha90 <msubhajitsahoo@gmail.com>
2018-01-10 14:11:19 +0530

    ne wfile added.

---
+++ Biggest.java
@@ -0,0 +1,10 @@
+class Biggest{
+public static void main(String[]args){
+int a=5,b=4,c=8,d=7,x,y, max;
+x=(a>b?a:b);
+y=(c>d?c:d);
+max=(x>y?x:y);
+System.out.println("Biggest no. is: "+max);
+}
+}
+
---
+++ monty.java
@@ -0,0 +1,23 @@
+class Monty{
+public static void main(String[]args){
+int a=57, b=68, c=46;
+if(a<b)
+{
+if (a<c){
+System.out.println("A is the smallest no.");
+}
+else
+{
+System.out.println("C is the smallest no.");
+}
+}
+else {
+if (b<c){
+System.out.println("B is the smallest no.");
+}
+else{
+System.out.println("C is the smallest no.");
+}
+}
+}
+}
